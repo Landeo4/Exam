5aabeb9843939df2c4e3a430f8bc81e36a32727e
tpotillion <theo91.pot@gmail.com>
2022-07-13 10:40:31 +0200

    envie de pénétré de la donzelle

--- aff_last_param/aff_last_param.c
+++ aff_last_param/aff_last_param.c
@@ -1,29 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   aff_last_param.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpotillion <tpotillion@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/12 10:15:40 by tpotillion        #+#    #+#             */
-/*   Updated: 2022/07/12 10:29:06 by tpotillion       ###   ########.fr       */
+/*   Created: 2022/07/13 10:24:35 by tpotillion        #+#    #+#             */
+/*   Updated: 2022/07/13 10:37:00 by tpotillion       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_strlen(char * str)
+int main(int argc, char *argv[])
 {
     int i = 0;
-    while(str[i])
+    if(argc < 1)
     {
+        write(1,"\n",1);
+        return 0;
+    }
+    while(argv[argc - 1][i])
+    {
+        write(1,&argv[argc - 1][i],1);
         i++;
     }
-    return i;
 }
-
-void ft_putstr(char *str)
-{
-    write(1,str,ft_strlen(str));
-}
-
